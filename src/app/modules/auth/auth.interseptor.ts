68cf3f6fcf6bc5f422105f2d837d7093feb7a62a
tahersami <taherrrsami@gmail.com>
2021-04-20 13:49:34 +0000

    complete translation

--- src/app/modules/auth/auth.interseptor.ts
+++ src/app/modules/auth/auth.interseptor.ts
@@ -1,15 +1,15 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse} from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
-import {Observable, throwError} from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: "root",
+  providedIn: "root",
 })
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private toaster: ToastrService) {}
+  constructor(private toaster: ToastrService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -33,9 +33,9 @@ export class AuthInterceptor implements HttpInterceptor {
       if (token != null) {
         request = request.clone({
           setHeaders: {
-              Authorization: 'Bearer ' + token.replace(/\"/g, ""),
+            Authorization: 'Bearer ' + token.replace(/\"/g, ""),
             // "X-Tenant": tanent,
-            "X-Tenant": 'hunter_allison_associates-',
+            "X-Tenant": 'carey_and_little_traders-',
           },
         });
       }
@@ -43,37 +43,37 @@ export class AuthInterceptor implements HttpInterceptor {
         request = request.clone({
           setHeaders: {
             // "X-Tenant": tanent,
-            "X-Tenant": 'hunter_allison_associates-',
+            "X-Tenant": 'carey_and_little_traders-',
           },
         });
       }
     }
 
     return next.handle(request)
-    .pipe(catchError(err => {
+      .pipe(catchError(err => {
         if ([401, 403].includes(err.status)) {
-            localStorage.removeItem('token');
-            localStorage.removeItem('permissions');
-            localStorage.removeItem('adminLogin');
-            window.location.href = ('auth/login');
+          localStorage.removeItem('token');
+          localStorage.removeItem('permissions');
+          localStorage.removeItem('adminLogin');
+          window.location.href = ('auth/login');
         }
 
-        if(err.error.validationErrors) {
+        if (err.error.validationErrors) {
           const error = err.error.validationErrors;
           error.map((item) => {
-              this.toaster.error(item.reason);
+            this.toaster.error(item.reason);
           })
           return throwError(error);
         }
-        else if(err.error.title) {
+        else if (err.error.title) {
           const error = err.error;
           this.toaster.error(err.error.title)
           return throwError(error);
         }
-        else if(err.error.detail) {
-            const error = err.error;
-            this.toaster.error(err.error.detail)
-            return throwError(error);
+        else if (err.error.detail) {
+          const error = err.error;
+          this.toaster.error(err.error.detail)
+          return throwError(error);
         }
         else {
           const error = err.error;
@@ -92,7 +92,7 @@ export class AuthInterceptor implements HttpInterceptor {
         //   const error =  err.statusText;
         //   return throwError(error);
         // }
-    }));
+      }));
   }
 
 }
--- src/app/pages/users/components/users-component/users-component.component.ts
+++ src/app/pages/users/components/users-component/users-component.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@an
 import { ToastrService } from 'ngx-toastr';
 import { LoaderService } from 'src/app/_metronic/core/services/loader.service';
 import { UsersService } from '../../users.service';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 
 
@@ -13,9 +13,9 @@ import { Router } from '@angular/router';
 })
 export class UsersListComponent implements OnInit {
   permissions = localStorage.getItem('permissions');
-  customActions:any[] = [];
-  selectedUserId:string;
-  dataSettings:any = {
+  customActions: any[] = [];
+  selectedUserId: string;
+  dataSettings: any = {
     selectedRolesIds: [],
     searchText: "",
     sortBy: "",
@@ -25,25 +25,25 @@ export class UsersListComponent implements OnInit {
   }
 
   displayedColumns: string[] = ['userName', 'email', 'isActive', 'createdOn'];
-  actions:any = [];
-  pagingData:any = {length: 100, pageSize: 10, pageIndex: 1};
-  gridData:any[];
+  actions: any = [];
+  pagingData: any = { length: 100, pageSize: 10, pageIndex: 1 };
+  gridData: any[];
 
   @ViewChild('deleteModal', { static: false }) deleteModal: ElementRef;
 
-  constructor(private usersService:UsersService, private toaster: ToastrService, private router: Router,
-              private loderService: LoaderService, private modalService: NgbModal) {}
+  constructor(private usersService: UsersService, private toaster: ToastrService, private router: Router,
+    private loderService: LoaderService, private modalService: NgbModal) { }
 
   getUsers() {
     this.loderService.setIsLoading = true;
     this.usersService.getUsers(this.dataSettings).subscribe((data) => {
-      if(data.result.users.items) {
+      if (data.result.users.items) {
         this.gridData = data.result.users.items.map((item) => {
-          if(item.isActive) {
+          if (item.isActive) {
             item.isActive = '<span class="label label-lg label-light-success label-inline">Active</span>';
           }
           else {
-            item.isActive = '<span class="label label-lg label-light-danger label-inline">'+item.isActive+'</span>';
+            item.isActive = '<span class="label label-lg label-light-danger label-inline">' + item.isActive + '</span>';
           }
           item.createdOn = this.getDateFormat(item.createdOn);
           return item;
@@ -57,23 +57,23 @@ export class UsersListComponent implements OnInit {
       this.pagingData.pageSize = data.result.users.totalRowsPerPage;
       this.pagingData.pageIndex = data.result.users.currentPage - 1;
       this.loderService.setIsLoading = false;
-    },(error) => {
+    }, (error) => {
       this.loderService.setIsLoading = false;
     })
   }
 
   getDateFormat(date) {
     var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+    if (month.length < 2)
+      month = '0' + month;
+    if (day.length < 2)
+      day = '0' + day;
 
-    return [ day, month, year,].join('/');
+    return [day, month, year,].join('/');
   }
 
   ngOnInit() {
@@ -83,32 +83,32 @@ export class UsersListComponent implements OnInit {
 
   checkPermissions() {
     // console.log(this.permissions);
-    if(this.permissions.includes('Users.GetUserPermissions')) {
-      this.customActions.push({name: 'permissions', icon:'flaticon2-lock text-success'})
+    if (this.permissions.includes('Users.GetUserPermissions')) {
+      this.customActions.push({ name: 'permissions', icon: 'flaticon2-lock text-success' })
     }
-    if(this.permissions.includes('Users.UpdateUser')) {
-      this.customActions.push({name: 'edit', icon:'flaticon2-edit text-warning'})
+    if (this.permissions.includes('Users.UpdateUser')) {
+      this.customActions.push({ name: 'edit', icon: 'flaticon2-edit text-warning' })
     }
-    if(this.permissions.includes('Users.DeleteUser')) {
-      this.customActions.push({name: 'delete', icon:'flaticon2-trash text-danger'})
+    if (this.permissions.includes('Users.DeleteUser')) {
+      this.customActions.push({ name: 'delete', icon: 'flaticon2-trash text-danger' })
     }
-    if(this.customActions.length > 0) {
+    if (this.customActions.length > 0) {
       this.displayedColumns.push('actions');
     }
   }
 
   actionsEvent(event) {
     this.selectedUserId = event.rowId;
-    if(event.type === 'view') {
+    if (event.type === 'view') {
       alert('view');
     }
-    else if(event.type === 'edit') {
+    else if (event.type === 'edit') {
       this.router.navigate([`/users/edit-user/${this.selectedUserId}`]);
     }
-    else if(event.type === 'delete') {
+    else if (event.type === 'delete') {
       this.openCentred(this.deleteModal);
     }
-    else if(event.type === 'permissions') {
+    else if (event.type === 'permissions') {
       this.router.navigate([`/users/edit-user-permissions/${this.selectedUserId}`]);
     }
   }
@@ -118,22 +118,22 @@ export class UsersListComponent implements OnInit {
     this.dataSettings.rowsPerPage = event.pageSize;
     this.getUsers();
   }
-  
+
   filterEvent(event) {
     this.dataSettings.pageNumber = 0;
     this.dataSettings.sortBy = "";
     this.dataSettings.searchText = event.filter;
     this.getUsers();
   }
-  
+
   sortEvent(event) {
-    this.dataSettings.sortBy = this.capitalizeFLetter(event.active)+' '+event.direction;
+    this.dataSettings.sortBy = this.capitalizeFLetter(event.active) + ' ' + event.direction;
     this.getUsers();
   }
 
-  capitalizeFLetter(value) { 
+  capitalizeFLetter(value) {
     var string = value;
-    return string[0].toUpperCase() + string.slice(1); 
+    return string[0].toUpperCase() + string.slice(1);
   }
 
   confirmDeleteUser() {
@@ -145,6 +145,6 @@ export class UsersListComponent implements OnInit {
   }
 
   openCentred(content) {
-    this.modalService.open(content, { centered: true } );
+    this.modalService.open(content, { centered: true });
   }
 }
